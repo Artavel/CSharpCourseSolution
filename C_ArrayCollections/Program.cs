8ab988e5c0e29aaf24ba8e29efbcf36f6bc75618
Alexander Velushov <User@USER-PC>
2019-08-26 22:00:45 +0300

    add Stack and Queue

--- C_ArrayCollections/Program.cs
+++ C_ArrayCollections/Program.cs
@@ -11,6 +11,112 @@ namespace C_ArraysCollections
             
         }
 
+        static void StackAndQueue()
+        {
+            var queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+
+            Console.WriteLine($"Should print out 1: {queue.Peek()}");
+
+            queue.Dequeue();
+
+            Console.WriteLine($"Should print out 2: {queue.Peek()}");
+            foreach (var cur in queue)
+            {
+                Console.WriteLine(cur);
+            }
+
+
+            Console.WriteLine();
+
+            var stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            Console.WriteLine($"Should print out 4: {stack.Peek()}");
+
+            stack.Pop();
+            Console.WriteLine($"Should print out 3: {stack.Peek()}");
+
+            Console.WriteLine("Iterate over the stack.");
+            foreach (var cur in stack)
+            {
+                Console.WriteLine(cur);
+            }
+        }
+
+        static void Dictionary()
+        {
+            var people = new Dictionary<int, string>();
+            people.Add(1, "John");
+            people.Add(2, "Bob");
+            people.Add(3, "Alice");
+
+            //or
+            people = new Dictionary<int, string>()
+            {
+                {1, "John"},
+                {2, "Bob"},
+                {3, "Alice"},
+            };
+
+            string name = people[1];
+            Console.WriteLine(name);
+
+            Console.WriteLine("Iterating over keys");
+            Dictionary<int, string>.KeyCollection keys = people.Keys;
+            foreach (var item in keys)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Iterating over values");
+            var values = people.Values;
+            foreach (var item in values)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Iterating over key-value pair");
+            foreach (var pair in people)
+            {
+                Console.WriteLine($"Key: {pair.Key}. Value: {pair.Value}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine($"Count = {people.Count}");
+
+            bool containsKey = people.ContainsKey(2);
+            bool containsValue = people.ContainsValue("John");
+
+            Console.WriteLine($"Contains key: {containsKey}. Contains value: {containsValue}");
+
+            people.Remove(1);
+
+            if (people.TryAdd(2, "Ellias"))
+            {
+                Console.WriteLine("Added successfully");
+            }
+            else
+            {
+                Console.WriteLine("Failed to add using key 2");
+            }
+
+            if (people.TryGetValue(2, out string val))
+            {
+                Console.WriteLine($"Key 2, val = {val}");
+            }
+            else
+            {
+                Console.WriteLine("Failed to get value");
+            }
+        }
+
         static void ListDemo()
         {
             var intList = new List<int>() { 1, 4, 2, 7, 5, 9, 12 };
