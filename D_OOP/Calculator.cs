171546ae3ef7e85bc0701b63d810bafd5764e5d2
Alexander Velushov <User@USER-PC>
2019-08-27 16:17:29 +0300

    add triangle calculator

---
+++ D_OOP/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D_OOP
+{
+    public class Calculator
+    {
+        public double CalcTriangleSquare(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        public double CalcTriangleSquare(double b, double h, int alpha)
+        {
+            double rads = alpha * Math.PI / 180;
+            return 0.5 * b * h * Math.Sin(rads);
+        }
+    }
+}
--- D_OOP/Program.cs
+++ D_OOP/Program.cs
@@ -6,10 +6,11 @@ namespace D_OOP
     {
         static void Main(string[] args)
         {
-            Caracter c = new Caracter();
-            c.Hit(120);
+            Calculator calc = new Calculator();
+            //double square1 = calc.CalcTriangleSquare(10, 20);
+            double square2 = calc.CalcTriangleSquare(15, 40, 20);
 
-            Console.WriteLine(c.Health);
+            Console.WriteLine($"Square2 = {square2}");
         }
     }
 }
