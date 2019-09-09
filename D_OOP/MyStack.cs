df67ab6465cf181897cdee2533eac460789df7a2
Alexander Velushov <User@USER-PC>
2019-09-09 13:21:26 +0300

    add E_Exceptions

--- D_OOP/MyStack.cs
+++ D_OOP/MyStack.cs
@@ -59,12 +59,7 @@ namespace D_OOP
 
             return _items[Count - 1];
         }
-
-        //public IEnumerator<T> GetEnumerator()
-        //{
-        //    return new StackEnumerator<T> (_items, Count);
-        //}
-
+        
         public IEnumerator<T> GetEnumerator()
         {
             for(int i  = Count -1; i >= 0; i--)
---
+++ E_Exceptions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace E_Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number");
+
+            string result = Console.ReadLine();
+
+            int number = 0;
+
+            try
+            {
+                number = int.Parse(result);
+            }
+
+            //Only for FormatException
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine("A format exception has occured.");
+            //    Console.WriteLine("Information is below:");
+            //    Console.WriteLine(ex.ToString());
+            //}
+
+            //For all Exceptions
+            catch (Exception ex)
+            {
+                Console.WriteLine("A format exception has occured.");
+                Console.WriteLine("Information is below:");
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
